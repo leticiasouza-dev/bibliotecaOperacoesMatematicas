a15f9ff9136c1591573f77cd8e59aab988a49ccb
leticiaSouzaDev <lec667121@gmail.com>
2023-12-07 17:24:21 -0300

    :tada:init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(operacoesMatematicas.adicao(100,100,150));
+        System.out.println(operacoesMatematicas.subtracao(8,3));
+        System.out.println(operacoesMatematicas.divisao(4,2));
+        System.out.println(operacoesMatematicas.multiplicacao(5, 2));
+
+        System.out.println(operacoesMatematicas.calculoAreaRetangulo(15, 5));
+        
+    }
+}
